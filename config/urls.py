43413bc6a15f2213049e31c4d7aba6980fa287a6
khxxn <kyohyun58@naver.com>
2026-06-11 22:06:06 +0900

    Refactor: "변경 URL 주소 체계 개선"
    - account/ space/ 주소 체계 변경
    - urls.py 개선

--- config/urls.py
+++ config/urls.py
@@ -3,20 +3,14 @@ from django.urls import path
 from app.views import (
     onboarding,
     onboarding_start,
-    space_main,
     space_room,
     memory_main,
     mypage_main,
 )
 from django.urls import include
 
-from spaces.views import (
-    home_main, 
-    space_create_step1, 
-    space_create_step2, 
-    space_create_step3, 
-    home_join_room
-)
+from spaces.views import home_main
+
 
 
 
@@ -25,14 +19,8 @@ urlpatterns = [
     path('', home_main, name='home_main'),
     path('onboarding/',onboarding, name='onboarding'),
     path('onboarding/start/', onboarding_start, name='onboarding_start'),
-    path('space/', space_main, name='space_main'),
     path('memory/', memory_main, name='memory_main'),
     path('mypage/', mypage_main, name='mypage_main'),
-    path('home/create-room/1/', space_create_step1, name='home_create_room1'),
-    path('home/create-room/2/', space_create_step2, name='home_create_room2'),
-    path('home/create-room/3/', space_create_step3, name='home_create_room3'),
-    path('home/join-room/', home_join_room, name='home_join_room'),
-    path('space/', space_main, name='space_main'),
-    path('space/room/', space_room, name='space_room'),
-    path('accounts/', include('accounts.urls')),
+    path('space/', include('spaces.urls')),
+    path('account/', include('accounts.urls')),
 ]
--- spaces/urls.py
+++ spaces/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
+from .views import *
+
+app_name = "spaces"
 
 urlpatterns = [
-    path('', views.home_main, name='home_main'),
-    path('create/step1/', views.space_create_step1, name='home_create_room1'),
-    path('create/step2/', views.space_create_step2, name='home_create_room2'),
-    path('create/step3/', views.space_create_step3, name='home_create_room3'),
-    path('join/', views.home_join_room, name='home_join_room'),
-    
+    path('', space_main, name='space_main'),
+    path('create-room/1/', space_create_step1, name='home_create_room1'),
+    path('create-room/2/', space_create_step2, name='home_create_room2'),
+    path('create-room/3/', space_create_step3, name='home_create_room3'),
+    path('join/', home_join_room, name='home_join_room'),
+    path('room/', space_room, name='space_room'),
 ]
