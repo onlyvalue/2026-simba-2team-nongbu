228925024ade4e4b981d6d0c8b3ba245a5b52011
onlyvalue <sarahkim2917@gmail.com>
2026-06-19 01:36:39 +0900

    Feat: space_upload form태그 추가 및 이미지 업로드 기능 구현

---
+++ static/js/space_upload.js
@@ -0,0 +1,31 @@
+const imageInput = document.getElementById("imageInput");
+const previewImage = document.getElementById("previewImage");
+const cameraIcon = document.getElementById("cameraIcon");
+const addImageBtn = document.querySelector(".add-image-btn");
+
+// 플러스 버튼 클릭 시 파일 선택창 열기
+addImageBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    imageInput.click();
+});
+
+// 파일 선택 시 미리보기
+imageInput.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        previewImage.src = e.target.result;
+
+        // 사진 표시
+        previewImage.style.display = "block";
+
+        // 카메라 아이콘 숨기기
+        cameraIcon.style.display = "none";
+    };
+
+    reader.readAsDataURL(file);
+});
